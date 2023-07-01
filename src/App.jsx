ae87d8340f5552bb3d07508343c6341d754009d2
Sanjay0712 <130839721+Sanjay0712@users.noreply.github.com>
2023-07-01 16:44:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,20 @@ const App = () => {
         {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
-          {/* <StarsCanvas /> */}
+          {/* <StarsCanvas /> */}   
         </div>
       </div>
       <div className="flex justify-center social-media bg-primary">
-          <a href="#"><i className='bx bxl-facebook'></i></a>
+          <a href="https://github.com/Sanjay0712" target="_blank"><i className='bx bxl-github'></i></a>
           <a href="#"><i className='bx bxl-twitter'></i></a>
           <a href="#"><i className='bx bxl-instagram-alt'></i></a>
           <a href="https://www.linkedin.com/in/sanjay-kumar-54b675160/" target="_blank"><i className='bx bxl-linkedin'></i></a>
       </div> 
+      <div className="flex justify-center bg-primary">
+        Copyright © 2023 Sanjay. All rights reserved.
+        <br />
+        <br />
+      </div>
     </Router>
   )
 }
