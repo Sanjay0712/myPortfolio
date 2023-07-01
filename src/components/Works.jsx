7bca61bb8113b17012420ecbc628ab94d4ac72c9
Sanjay0712 <130839721+Sanjay0712@users.noreply.github.com>
2023-07-01 16:48:13 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,6 +5,44 @@ import { github } from '../assets';
 import SectionWrapper from '../hoc/SectionWrapper';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import React, { useState } from 'react';
+import '../carouselStyle.css';
+import { TiChevronLeftOutline, TiChevronRightOutline} from 'react-icons/ti';
+
+const CARDS = projects.length;
+const MAX_VISIBILITY = 3;
+
+const Card = ({title, content}) => (
+  <div className='card'>
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
+);
+
+const Carousel = ({children}) => {
+  const [active, setActive] = useState(2);
+  const count = React.Children.count(children);
+  
+  return (
+    <div className='carousel'>
+      {active > 0 && <button className='left' onClick={() => setActive(i => i - 1)}><TiChevronLeftOutline/></button>}
+      {React.Children.map(children, (child, i) => (
+        <div className='card-container' style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+          }}>
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && <button className='right' onClick={() => setActive(i => i + 1)}><TiChevronRightOutline/></button>}
+    </div>
+  );
+};
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
@@ -51,26 +89,37 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>My work</p>
-        <h2 className={styles.sectionHeadText}>Projects</h2>
+        {/* <p className={styles.sectionSubText}>My work</p> */}
+        <h2 style={{textAlign: "center", margin: "20px"}} className={styles.sectionHeadText}>Projects</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className='w-full flex ml-40 mb-10' style={{textAlign: "center"}}>
         <motion.p 
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-        >
+        > 
           Following projects showcases my skills and Experience through real-world examples of my work. Each project is briefly described with links to code 
           repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects
           effectively.
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7 '>
+      {/* <div className='mt-20 flex flex-wrap gap-7 '>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
+      </div> */}
+
+      <div className='carouselMain flex justify-center mr-80'>
+        <Carousel>
+          {projects.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          ))}
+        </Carousel>
       </div>
+      <br />
+      <br />
+      <br />
     </>
   )
 }
